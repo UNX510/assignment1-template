5ca034c7274db1fb027684230a03c679d52552b3
Chris Johnson <chris.johnson@senecacollege.ca>
2024-06-21 10:47:46 -0400

    Initial template scaffolding.

---
+++ q3.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#####################################################################################
+# UNX510 – Assignment 1                                                             #
+# I declare that this assignment is my own work in accordance with Seneca Academic  #
+# Policy.                                                                           #
+#                                                                                   #
+# No part of this assignment has been copied manually or electronically from any    #
+# other source (including web sites) or distributed to other students.              #
+#                                                                                   #
+# Name: __________________ Student ID: ______________ Date: __________________      #
+#                                                                                   #
+#####################################################################################
+
+# Purpose: You are asked to develop a script which checks whether two given files (contents) are
+# identical.
+
+# Requirements:
+#
+# 1. Check whether user provided the correct number of arguments. Display proper error message if
+# required.
+#
+# 2. Check whether both filenames exist. Display proper error message if required.
+#
+# 3. Calculate the number of bytes/characters in each file and if the number of characters in both files
+# are the same, the script displays “Files are similar”. Otherwise it displays “Files are different.”
+#
+# Tip: Make sure to handle errors, check file existence, and validate input/arguments.
+
+# Usage: ./q3.bash filename1 filename2
